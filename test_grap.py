d5a84d2f279c5cfed90bee272b7df6337df9c6db
A-Lan-08 <adlanecki@outlook.com>
2025-10-23 12:42:22 +0100

    Working on buttons on the left

--- test_grap.py
+++ test_grap.py
@@ -1,10 +1,14 @@
+
+
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QColor, QPalette, QPainter, QPixmap, QPainterPath
+
+from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtGui import QColor, QPalette, QPainter, QPixmap, QPainterPath, QBrush
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout,
-    QLabel, QFrame
+    QLabel, QPushButton, QFrame
 )
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,40 +16,73 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Dashboard Layout Example")
         self.setGeometry(100, 100, 1500, 900)
 
+        ##### --- Main alignment --- #####
         central = QWidget()
         self.setCentralWidget(central)
-        main_layout = QHBoxLayout(central)
 
-        left_bar = self.coloured_frame("lightgray")
-        left_layout = QVBoxLayout(left_bar)
+        main_layout = QHBoxLayout()
+        central.setLayout(main_layout)
+
+        ##### --- Left sidebar --- #####
+        left_frame = QFrame()
+        left_frame.setStyleSheet("border: 1px solid black;")
+
+        left_layout = QVBoxLayout(left_frame)
+        left_layout.setContentsMargins(0,0,0,0)#1,1,1,1)
+        left_layout.setSpacing(1)
+
+        left_btn_heights = 100
+
+        mouse_btn = QPushButton("Mouse")
+        # mouse_img = QPixmap('mouse.jpg').scaled(80,100)
+        # mouse_img.save("mouse_scaled.jpg")
+        self.scale_img("mouse.jpg", "mouse_scaled.jpg", 80, 100)
+        mouse_btn.setStyleSheet("""
+        QPushButton {background-image: url('mouse_scaled.jpg'); background-repeat: no-repeat; background-position: centre; background-size: contain;}
+        QPushButton:hover {background-color: #34abeb} 
+        QPushButton:pressed {background-color: #1338bd; color: white} 
+        """)
+        mouse_btn.setFixedHeight(left_btn_heights)
+
+        # mouse_btn.clicked.connect(self.mouse_tool)
+
+        # line_tool = QPushButton()
+        # line_tool.setFixedHeight(left_btn_heights)
+
+        left_layout.addWidget(mouse_btn)
+        # left_layout.addWidget(line_tool)
         left_layout.addStretch()
 
+        ##### --- Center bar --- #####
         center_frame = QFrame()
         center_layout = QVBoxLayout(center_frame)
 
-        top_bar = self.coloured_frame("whitesmoke")
-        top_layout = QHBoxLayout(top_bar)
-        top_layout.addStretch()
+        # Top centre
+        top_centre = self.coloured_frame("transparent")
+        top_centre_layout = QHBoxLayout(top_centre)
+        top_centre_layout.addStretch()
 
-        graph_frame = self.coloured_frame("white")
-        graph_layout = QVBoxLayout(graph_frame)
+        # Graph area
+        graph_frame = self.coloured_frame("transparent")
         graph_label = QLabel("Graph Area")
         graph_label.setAlignment(Qt.AlignCenter)
-        graph_layout.addWidget(graph_label)
+        graph_frame.layout().addWidget(graph_label)
 
-        center_layout.addWidget(top_bar, 1)
+        center_layout.addWidget(top_centre, 1)
         center_layout.addWidget(graph_frame, 10)
 
+        ##### --- Right sidebar --- #####
         right_frame = QFrame()
         right_layout = QVBoxLayout(right_frame)
 
-        profile_bar = QWidget()
-        profile_bar.setStyleSheet("background-color: lightgray;")
-        right_bar_profile_layout = QVBoxLayout(profile_bar)
-        right_bar_profile_layout.setAlignment(Qt.AlignCenter)
+        # Profile screen
+        profile_frame = QWidget()
+        profile_frame.setStyleSheet("background-color: None;")
+        profile_frame_layout = QVBoxLayout(profile_frame)
+        profile_frame_layout.setAlignment(Qt.AlignCenter)
 
         circle_label = QLabel()
-        pixmap = QPixmap("img.jpg")
+        pixmap = QPixmap("person_icon.jpg")
         if pixmap.isNull():
             pixmap = QPixmap(200, 200)
             pixmap.fill(Qt.gray)
@@ -53,21 +90,24 @@ class MainWindow(QMainWindow):
         circle_label.setPixmap(circle_pixmap)
         circle_label.setAlignment(Qt.AlignCenter)
 
-        right_bar_profile_layout.addWidget(circle_label, alignment=Qt.AlignCenter)
+        profile_frame_layout.addWidget(circle_label, alignment=Qt.AlignCenter)
 
-        right_bar_prediction_settings = self.coloured_frame("red")
+        # Prediction settings frame
+        prediction_settings_frame = self.coloured_frame("transparent")
 
-        right_bar_prediction = self.coloured_frame("lightgray")
-        right_bar_prediction_layout = QVBoxLayout(right_bar_prediction)
-        right_bar_label = QLabel("Prediction result")
-        right_bar_label.setAlignment(Qt.AlignCenter)
-        right_bar_prediction_layout.addWidget(right_bar_label)
+        # Prediction result
+        prediction_result_frame = self.coloured_frame("transparent")
+        prediction_result_label = QLabel("Prediction result")
+        prediction_result_label.setAlignment(Qt.AlignCenter)
+        prediction_result_frame.layout().addWidget(prediction_result_label)
 
-        right_layout.addWidget(profile_bar, 1)
-        right_layout.addWidget(right_bar_prediction_settings, 10)
-        right_layout.addWidget(right_bar_prediction, 10)
 
-        main_layout.addWidget(left_bar, 1)
+        right_layout.addWidget(profile_frame, 1)
+        right_layout.addWidget(prediction_settings_frame, 10)
+        right_layout.addWidget(prediction_result_frame, 10)
+
+        ##### --- Add to layout --- #####
+        main_layout.addWidget(left_frame, 1)
         main_layout.addWidget(center_frame, 15)
         main_layout.addWidget(right_frame, 3)
 
@@ -80,19 +120,30 @@ class MainWindow(QMainWindow):
         frame.setPalette(palette)
         if min_height:
             frame.setMinimumHeight(min_height)
+        layout = QVBoxLayout(frame)
+        layout.setContentsMargins(5, 5, 5, 5)
         return frame
 
     def circle_bitmap(self, pixmap, diameter):
         pixmap = pixmap.scaled(diameter, diameter, Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
+
         mask = QPixmap(diameter, diameter)
         mask.fill(Qt.transparent)
+
         painter = QPainter(mask)
         path = QPainterPath()
         path.addEllipse(0, 0, diameter, diameter)
         painter.setClipPath(path)
+
         painter.drawPixmap(0, 0, pixmap)
         painter.end()
         return mask
+
+    def scale_img(self, old_path, new_path, x, y):
+        print("used")
+        img = QPixmap(old_path).scaled(x,y)
+        img.save("mo.jpg")#new_path)
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
